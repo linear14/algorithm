a8c99c14451406ae8074551bb029f71af7318ec8
linear14 <47495592+linear14@users.noreply.github.com>
2020-04-12 23:52:34 +0900

    10987 - 모음의 개수

---
+++ src/algorithm_beakjoon/.10987_모음의 개수.kt
@@ -0,0 +1,14 @@
+package algorithm_beakjoon
+
+// 200412
+// 10987 : 모음의 개수
+
+fun main() {
+    val input = readLine()!!
+    var count = 0
+    var list = arrayOf('a','e','i','o','u')
+    for(i in input) {
+        if(i in list) count++
+    }
+    print(count)
+}
