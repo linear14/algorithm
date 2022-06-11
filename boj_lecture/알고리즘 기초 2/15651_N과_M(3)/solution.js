a2c15835d546ad66f79b84713ca63e7589eeaffa
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-06-11 12:17:16 +0900

    [boj-lecture] 백준 15651 - N과 M (3)

---
+++ boj_lecture/알고리즘 기초 2/15651_N과_M(3)/solution.js
@@ -0,0 +1,30 @@
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let n, m;
+
+rl.on('line', (input) => {
+  [n, m] = input.split(' ').map(i => +i);
+  rl.close();
+}).on('close', () => {
+  const ans = [];
+  
+  const arr = [];
+  const bt = (depth) => {
+    if(depth === m) {
+      ans.push(arr.join(' '));
+      return;
+    }
+
+    for(let i = 1; i <= n; i++) {
+      arr.push(i);
+      bt(depth + 1);
+      arr.pop(i);
+    }
+  }
+
+  bt(0);
+  console.log(ans.join('\n'));
+});
