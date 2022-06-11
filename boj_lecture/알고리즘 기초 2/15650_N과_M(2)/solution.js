3f32dead74092c19adc0d5e5fc045a1bc4fc08d5
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2022-06-11 12:09:05 +0900

    [boj-lecture] 백준 15650 - N과 M (2)

---
+++ boj_lecture/알고리즘 기초 2/15650_N과_M(2)/solution.js
@@ -0,0 +1,32 @@
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let n, m;
+
+rl.on('line', (input) => {
+  [n, m] = input.split(' ').map(i => +i);
+  rl.close();  
+}).on('close', () => {
+  const ans = [];
+  const arr = [];
+  
+  const bt = (depth) => {
+    if(depth === m) {
+      ans.push(arr.join(' '));
+      return;
+    }
+
+    for(let i = 1; i <= n; i++) {
+      if(depth === 0 || arr[depth - 1] < i) {
+        arr.push(i);
+        bt(depth + 1);
+        arr.pop();
+      }
+    }
+  }
+
+  bt(0);
+  console.log(ans.join('\n'));
+});
